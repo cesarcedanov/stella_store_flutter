36c548ed7a59a6752b65402ff491ea6ea9d20e5c
CesarCedanoV <CesarCedanoV@gmail.com>
2019-10-16 12:46:29 -0400

    Set an auto logout function when the auth expiry

--- stella_store/lib/providers/auth.dart
+++ stella_store/lib/providers/auth.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:async';
 import '../models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
+  Timer _authTimer;
 
   bool get isAuth {
     return token != null;
@@ -50,6 +52,7 @@ class Auth with ChangeNotifier {
           ),
         ),
       );
+      _autoLogout();
       notifyListeners();
     } catch (error) {
       throw error;
@@ -68,10 +71,22 @@ class Auth with ChangeNotifier {
     return _authenticate(email, password, url);
   }
 
-  Future<void> logout() {
+  void logout() {
     _token = null;
     _userId = null;
     _expiryDate = null;
+    if (_authTimer != null) {
+      _authTimer.cancel();
+      _authTimer = null;
+    }
     notifyListeners();
+  }
+
+  void _autoLogout() {
+    if (_authTimer != null) {
+      _authTimer.cancel();
+    }
+    final timeToExpiry = _expiryDate.difference(DateTime.now()).inSeconds;
+    _authTimer = Timer(Duration(seconds: timeToExpiry), logout);
   }
 }
